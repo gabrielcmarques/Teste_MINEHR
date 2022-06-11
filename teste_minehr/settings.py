e3267459c2491532ce7992811c9322d86d78dad6
Gabriel C. Marques <gabrielchristomarques@gmail.com>
2022-06-11 00:17:23 -0300

    fixed secret key

--- teste_minehr/settings.py
+++ teste_minehr/settings.py
@@ -9,7 +9,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-xy1=!b4e!x9741z0_ibefeg%&=pr9q$31wnf+(#van-%@zce2^'
+SECRET_KEY = os.environ.get('DJ_SECRET')
+
 
 # SECURITY WARNING: don't run with debug turned on in production!
 
